9cc9e92c917383fc2614c2319dce18274a9e9cbd
rejasupotaro <takiguchi0817@gmail.com>
2017-11-12 16:43:37 +0000

    Create LogicGate

---
+++ logic-gate-android/LogicGate/app/src/main/java/rejasupotaro/logicgate/LogicGate.kt
@@ -0,0 +1,30 @@
+package rejasupotaro.logicgate
+
+import android.content.res.AssetManager
+import org.tensorflow.contrib.android.TensorFlowInferenceInterface
+
+class LogicGate(assets: AssetManager, private val logger: (String) -> Unit = {}) {
+    private val inferenceInterface = TensorFlowInferenceInterface(assets, "xor_keras.pb")
+    private val inputName = "dense_1_input_1"
+    private val outputName = "output_0"
+
+    init {
+        for (op in inferenceInterface.graph().operations()) {
+            logger.invoke("name: ${op.name()}, type: ${op.type()}, numOutputs: ${op.numOutputs()}")
+        }
+    }
+
+    fun xor(x1: Int, x2: Int): Int {
+        val input = floatArrayOf(x1.toFloat(), x2.toFloat())
+        inferenceInterface.feed(inputName, input, 1, 2)
+        inferenceInterface.run(arrayOf(outputName))
+
+        val output = FloatArray(1)
+        inferenceInterface.fetch(outputName, output)
+
+        logger.invoke("DONE")
+
+        return if (output[0] < 0.5) 0 else 1
+    }
+}
+
--- logic-gate-android/LogicGate/app/src/main/java/rejasupotaro/logicgate/MainActivity.kt
+++ logic-gate-android/LogicGate/app/src/main/java/rejasupotaro/logicgate/MainActivity.kt
@@ -1,12 +1,15 @@
 package rejasupotaro.logicgate
 
-import android.support.v7.app.AppCompatActivity
 import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+import android.widget.Toast
 
 class MainActivity : AppCompatActivity() {
 
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         setContentView(R.layout.activity_main)
+
+        Toast.makeText(this, LogicGate(assets).xor(1, 0).toString(), Toast.LENGTH_SHORT).show()
     }
 }
