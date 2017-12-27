9f1d031f5309285464530cc04c5d09797444dda1
rejasupotaro <takiguchi0817@gmail.com>
2017-12-27 12:26:37 +0000

    Make code a bit nicer

--- logic-gate-android/app/src/main/java/rejasupotaro/logicgate/LogicGate.kt
+++ logic-gate-android/app/src/main/java/rejasupotaro/logicgate/LogicGate.kt
@@ -5,8 +5,11 @@ import org.tensorflow.contrib.android.TensorFlowInferenceInterface
 
 class LogicGate(assets: AssetManager, private val logger: (String) -> Unit = {}) {
     private val inferenceInterface = TensorFlowInferenceInterface(assets, "xor_keras.pb")
+    private val inputShape = Pair(1, 2)
     private val inputName = "dense_1_input_1"
+    private val outputShape = Pair(1, 1)
     private val outputName = "output_0"
+    private val output = FloatArray(outputShape.first * outputShape.second)
 
     init {
         for (op in inferenceInterface.graph().operations()) {
@@ -16,10 +19,9 @@ class LogicGate(assets: AssetManager, private val logger: (String) -> Unit = {})
 
     fun xor(x1: Int, x2: Int): Int {
         val input = floatArrayOf(x1.toFloat(), x2.toFloat())
-        inferenceInterface.feed(inputName, input, 1, 2)
-        inferenceInterface.run(arrayOf(outputName))
 
-        val output = FloatArray(1)
+        inferenceInterface.feed(inputName, input, inputShape.first.toLong(), inputShape.second.toLong())
+        inferenceInterface.run(arrayOf(outputName))
         inferenceInterface.fetch(outputName, output)
 
         logger("input: [$x1, $x2] => output (probability): ${output[0]}")
