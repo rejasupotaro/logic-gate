38ee5a4fe26867bcf4426cf5a82ca5c6020c8900
rejasupotaro <takiguchi0817@gmail.com>
2017-12-27 21:09:34 +0000

    Write tests

---
+++ logic-gate-android/app/src/androidTest/java/rejasupotaro/logicgate/LogicGateTest.kt
@@ -0,0 +1,28 @@
+package rejasupotaro.logicgate
+
+import android.support.test.InstrumentationRegistry
+import android.support.test.runner.AndroidJUnit4
+import org.junit.Assert.assertEquals
+import org.junit.Test
+import org.junit.runner.RunWith
+
+@RunWith(AndroidJUnit4::class)
+class LogicGateTest {
+    private val logicGate by lazy {
+        val assetManager = InstrumentationRegistry.getTargetContext().assets
+        LogicGate(assetManager)
+    }
+
+    @Test
+    fun and() {
+        listOf(
+                Pair(Pair(0, 0), 0),
+                Pair(Pair(0, 1), 0),
+                Pair(Pair(1, 0), 0),
+                Pair(Pair(1, 1), 1)
+        ).forEach { (input, output) ->
+            val actual = logicGate.and(input.first, input.second)
+            assertEquals(output, actual)
+        }
+    }
+}
--- logic-gate-android/app/src/androidTest/java/rejasupotaro/logicgate/MainViewModelTest.kt
+++ logic-gate-android/app/src/androidTest/java/rejasupotaro/logicgate/MainViewModelTest.kt
@@ -4,10 +4,10 @@ import android.arch.lifecycle.Observer
 import android.support.test.rule.ActivityTestRule
 import android.support.test.runner.AndroidJUnit4
 import com.nhaarman.mockito_kotlin.mock
+import org.junit.Assert.assertEquals
 import org.junit.Rule
 import org.junit.Test
 import org.junit.runner.RunWith
-
 
 @RunWith(AndroidJUnit4::class)
 class MainViewModelTest {
@@ -20,8 +20,8 @@ class MainViewModelTest {
         val viewModel = MainViewModel(activityRule.activity.application)
         val observer: Observer<Int> = mock()
         viewModel.output.observeForever(observer)
-        assert(viewModel.output.value == 0)
+        assertEquals(viewModel.output.value, 0)
         viewModel.infer(1, 0)
-        assert(viewModel.output.value == 1)
+        assertEquals(viewModel.output.value, 1)
     }
 }
