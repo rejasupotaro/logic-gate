273b6f98e99220c9eae771746461c24a9e04126c
rejasupotaro <takiguchi0817@gmail.com>
2017-12-28 07:53:27 +0000

    Reuse allocated memory

--- logic-gate-android/app/src/main/java/rejasupotaro/logicgate/LogicGate.kt
+++ logic-gate-android/app/src/main/java/rejasupotaro/logicgate/LogicGate.kt
@@ -7,6 +7,7 @@ class LogicGate(assets: AssetManager, private val logger: (String) -> Unit = {})
     private val inferenceInterface = TensorFlowInferenceInterface(assets, "optimized_logic_and_gate.pb")
     private val inputShape = Pair(1, 2)
     private val inputName = "x"
+    private val input = FloatArray(inputShape.first * inputShape.second)
     private val outputShape = Pair(1, 1)
     private val outputName = "y_pred"
     private val output = FloatArray(outputShape.first * outputShape.second)
@@ -19,7 +20,8 @@ class LogicGate(assets: AssetManager, private val logger: (String) -> Unit = {})
     }
 
     fun and(x1: Int, x2: Int): Int {
-        val input = floatArrayOf(x1.toFloat(), x2.toFloat())
+        input[0] = x1.toFloat()
+        input[1] = x2.toFloat()
 
         inferenceInterface.feed(inputName, input, inputShape.first.toLong(), inputShape.second.toLong())
         inferenceInterface.run(arrayOf(outputName))
